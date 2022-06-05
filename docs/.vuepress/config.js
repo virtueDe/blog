432ca5d1d75f91d60f512bf4ca7fcf83d9b59a77
xiaoling <xiaoling@laihua.com>
2022-06-05 15:44:19 +0800

    feat: add search

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,20 @@
 const { defaultTheme } = require('vuepress')
+const { searchPlugin } = require('@vuepress/plugin-search')
 const navbar = require('./config/navbar')
 const sidebar = require('./config/sidebar')
 module.exports = {
   base: '/blog/',
   lang: "zh-CN",
-  title: 'blog',
-  description: 'virtue的blog',
+  title: '前端笔记',
+  description: 'virtue的前端笔记',
   theme: defaultTheme({
     sidebar,
     navbar,
     sidebarDepth: 2, // 侧边栏显示2级;
-  })
+  }),
+  plugins: [
+    searchPlugin({
+      // 配置项
+    }),
+  ],
 }
